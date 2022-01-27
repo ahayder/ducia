a0e428f878dc9eae80d59dce476e043bca14e124
Ali Hayder <ahayder@Alis-MacBook-Air.local>
2022-01-27 06:06:38 +0600

    Remove Bad Css

--- components/LandingPage/Header.js
+++ components/LandingPage/Header.js
@@ -4,13 +4,7 @@ const Header = () => {
     const repeater = 300;
   return (
     <>
-        <div className="wrap">
-            <ul className="uordList">
-                {Array(repeater).map((_, i) => {
-                    <li key={i} className="list p"></li>
-                })}
-            </ul>
-            </div>
+        
   </>
   );
 };
